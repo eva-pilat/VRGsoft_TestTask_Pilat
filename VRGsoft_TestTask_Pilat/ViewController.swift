f158b75bcefe336f95429ae246268641ddcf7e34
Eva Pilat <evissdun@icloud.com>
2025-11-03 11:58:32 +0200

    Initial Commit

---
+++ VRGsoft_TestTask_Pilat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VRGsoft_TestTask_Pilat
+//
+//  Created by Єва Матвєєва on 03.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
